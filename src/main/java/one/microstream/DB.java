0ccdd9eda9dd1fc9ef20e260c7dfe4f208cfe4ae
Kümmel Christian <c.kuemmel@microstream.one>
2021-09-15 10:51:34 +0200

    zwischencommit

--- src/main/java/one/microstream/DB.java
+++ src/main/java/one/microstream/DB.java
@@ -1,10 +1,15 @@
 package one.microstream;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import io.micronaut.core.io.ResourceResolver;
 import io.micronaut.core.io.scan.ClassPathResourceLoader;
+import one.microstream.chars.XChars;
+import one.microstream.exceptions.IORuntimeException;
 import one.microstream.persistence.types.Persistence;
 import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
 import one.microstream.storage.embedded.types.EmbeddedStorageManager;
@@ -17,16 +22,25 @@ public class DB
 	
 	static
 	{
-		ClassPathResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
-		Optional<URL> resource = loader.getResource("microstream.xml");
+		final ClassPathResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
+		final Optional<URL> resource = loader.getResource("microstream.xml");
 		
-		ClassPathResourceLoader refactoringLoader =
+		final ClassPathResourceLoader refactoringLoader =
 			new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
-		Optional<URL> refactoringResource = refactoringLoader.getResource("refactoring.csv");
+		
+		String mapping;
+		try(InputStream in = refactoringLoader.getResourceAsStream("refactoring.csv").get())
+		{
+			mapping = XChars.readStringFromInputStream(in, StandardCharsets.UTF_8);
+		}
+		catch(final IOException e)
+		{
+			throw new IORuntimeException(e);
+		}
 		
 		storageManager = EmbeddedStorageConfiguration.load(
 			resource.get().getPath()).createEmbeddedStorageFoundation().setRefactoringMappingProvider(
-				Persistence.RefactoringMapping(refactoringResource.get().getPath())).createEmbeddedStorageManager(
+				Persistence.RefactoringMapping(mapping)).createEmbeddedStorageManager(
 					root).start();
 		
 	}
