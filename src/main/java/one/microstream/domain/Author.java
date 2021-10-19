2f3d0e34a711aa1861ee93912dda11a2f13ee13c
Kümmel Christian <c.kuemmel@microstream.one>
2021-10-20 00:30:51 +0200

    zc

--- src/main/java/one/microstream/domain/Author.java
+++ src/main/java/one/microstream/domain/Author.java
@@ -2,20 +2,70 @@ package one.microstream.domain;
 
 public class Author
 {
-	private String	name_2;
-	private String	name_3;
-	private String	name_4;
-	private String	name_5;
-	private String	name_6;
+	private String	mail_2;
+	private String	mail_3;
+	private String	mail_4;
+	private String	mail_5;
+	private String	mail_6;
 	private String	firstname;
 	private String	name;
 	
-	public Author(String name_6, String firstname, String name)
+	public Author(String mail_6, String firstname, String name)
 	{
 		super();
-		this.name_6 = name_6;
+		this.mail_6 = mail_6;
 		this.firstname = firstname;
 		this.name = name;
+	}
+	
+	public String getMail_2()
+	{
+		return mail_2;
+	}
+	
+	public void setMail_2(String mail_2)
+	{
+		this.mail_2 = mail_2;
+	}
+	
+	public String getMail_3()
+	{
+		return mail_3;
+	}
+	
+	public void setMail_3(String mail_3)
+	{
+		this.mail_3 = mail_3;
+	}
+	
+	public String getMail_4()
+	{
+		return mail_4;
+	}
+	
+	public void setMail_4(String mail_4)
+	{
+		this.mail_4 = mail_4;
+	}
+	
+	public String getMail_5()
+	{
+		return mail_5;
+	}
+	
+	public void setMail_5(String mail_5)
+	{
+		this.mail_5 = mail_5;
+	}
+	
+	public String getMail_6()
+	{
+		return mail_6;
+	}
+	
+	public void setMail_6(String mail_6)
+	{
+		this.mail_6 = mail_6;
 	}
 	
 	public String getFirstname()
@@ -36,56 +86,6 @@ public class Author
 	public void setName(String name)
 	{
 		this.name = name;
-	}
-	
-	public String getName_2()
-	{
-		return name_2;
-	}
-	
-	public void setName_2(String name_2)
-	{
-		this.name_2 = name_2;
-	}
-	
-	public String getName_3()
-	{
-		return name_3;
-	}
-	
-	public void setName_3(String name_3)
-	{
-		this.name_3 = name_3;
-	}
-	
-	public String getName_4()
-	{
-		return name_4;
-	}
-	
-	public void setName_4(String name_4)
-	{
-		this.name_4 = name_4;
-	}
-	
-	public String getName_5()
-	{
-		return name_5;
-	}
-	
-	public void setName_5(String name_5)
-	{
-		this.name_5 = name_5;
-	}
-	
-	public String getName_6()
-	{
-		return name_6;
-	}
-	
-	public void setName_6(String name_6)
-	{
-		this.name_6 = name_6;
 	}
 	
 }
