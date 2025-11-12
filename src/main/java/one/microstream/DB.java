011c4274313acd9280ce9dfc5de7bbad9fefef99
Chrstian Kümmel <c.kuemmel@microstream.one>
2025-11-12 14:55:09 +0100

    Upgrade to latest ES Version

--- src/main/java/one/microstream/DB.java
+++ src/main/java/one/microstream/DB.java
@@ -5,16 +5,15 @@ import java.util.Optional;
 
 import io.micronaut.core.io.ResourceResolver;
 import io.micronaut.core.io.scan.ClassPathResourceLoader;
-import one.microstream.persistence.internal.InquiringLegacyTypeMappingResultor;
-import one.microstream.persistence.types.PersistenceLegacyTypeMappingResultor;
-import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
-import one.microstream.storage.embedded.types.EmbeddedStorageFoundation;
-import one.microstream.storage.embedded.types.EmbeddedStorageManager;
-
+import org.eclipse.serializer.persistence.types.LoggingLegacyTypeMappingResultor;
+import org.eclipse.serializer.persistence.types.PersistenceLegacyTypeMappingResultor;
+import org.eclipse.store.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
+import org.eclipse.store.storage.embedded.types.EmbeddedStorageFoundation;
+import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager;
 
 public class DB
 {
-	public static EmbeddedStorageManager	storageManager;
+	public static EmbeddedStorageManager storageManager;
 	public final static DataRoot			root	= new DataRoot();
 	
 	static
@@ -24,10 +23,9 @@ public class DB
 		
 		EmbeddedStorageFoundation<?> foundation = EmbeddedStorageConfiguration.load(
 			resource.get().getPath()).createEmbeddedStorageFoundation();
-		
-		foundation.getConnectionFoundation().setLegacyTypeMappingResultor(
-			InquiringLegacyTypeMappingResultor.New(
-				PersistenceLegacyTypeMappingResultor.New()));
+
+		foundation.getConnectionFoundation().setLegacyTypeMappingResultor(LoggingLegacyTypeMappingResultor.New(
+                PersistenceLegacyTypeMappingResultor.New()));
 		
 		storageManager = foundation.createEmbeddedStorageManager(root).start();
 	}
